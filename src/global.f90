e4aa96d749feda31293db8f716f27bc19600c72f
lbusschaert <louise.busschaert@kuleuven.be>
2021-12-15 16:04:58 +0100

    ENH: convert HarvestIndexGrowthCoefficient to Fortran

--- src/global.f90
+++ src/global.f90
@@ -303,5 +303,30 @@ real(dp) function GetWeedRC(TheDay, GDDayi, fCCx, TempWeedRCinput, TempWeedAdj,&
     GetWeedRC = WeedRCDayCalc
 end function GetWeedRC
 
+real(dp) function HarvestIndexGrowthCoefficient(HImax, dHIdt)
+    real(dp), intent(in) :: HImax
+    real(dp), intent(in) :: dHIdt
+    real(dp) :: HIo, HIvar, HIGC, t
+
+    HIo = 1
+
+    if (HImax > HIo) then
+        t = HImax/dHIdt
+        HIGC = 0.001
+        HIvar = (HIo*HImax)/(HIo+(HImax-HIo)*exp(-HIGC*t))
+        do while (HIvar <= (0.98*HImax))
+            HIGC = HIGC + 0.001
+            HIvar = (HIo*HImax)/(HIo+(HImax-HIo)*exp(-HIGC*t))
+        end do
+
+        if (HIvar >= HImax) then
+            HIGC = HIGC - 0.001
+        end if
+    else
+        HIGC = undef_int
+    end if
+    HarvestIndexGrowthCoefficient = HIGC
+
+end function HarvestIndexGrowthCoefficient
 
 end module ac_global
