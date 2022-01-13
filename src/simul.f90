2a901df5ba13389ca02264a8ab54f10ca3ee890d
vsc33651 <vsc33651@tier2-p-login-4.genius.hpc.kuleuven.be>
2022-01-13 11:26:18 +0100

    Convert GetCDCadjustedNoStressNew to Fortran

--- src/simul.f90
+++ src/simul.f90
@@ -1,11 +1,27 @@
 module ac_simul
 
-use ac_kinds, only: int32
+use ac_kinds, only:  dp, &
+                    int8, &
+                    int32, &
+                    sp
 implicit none
+
 
 
 contains
 
 
+real(dp) function GetCDCadjustedNoStressNew(CCx, CDC, CCxAdjusted)
+    real(dp), intent(in) :: CCx
+    real(dp), intent(in) :: CDC
+    real(dp), intent(in) :: CCxAdjusted
+
+    real(dp) :: CDCadjusted
+
+    ! CDCadjusted := CDC * (CCxadjusted/CCx);
+    CDCadjusted = CDC * ((CCxadjusted+2.29_dp)/(CCx+2.29_dp))
+    GetCDCadjustedNoStressNew = CDCadjusted
+    
+end function GetCDCadjustedNoStressNew
 
 end module ac_simul
