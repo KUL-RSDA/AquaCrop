b3f677e49284c6cfdef4dcacef11ccdfc494ccba
Michel Bechtold <michel.bechtold@gmx.de>
2022-01-21 19:08:29 +0100

    Revert "WIP: global variables TemperatureFile(full) and TemperatureRecord + c…"

--- src/interface_tempprocessing.pas
+++ src/interface_tempprocessing.pas
@@ -5,28 +5,13 @@ interface
 uses Global, interface_global;
 
 
-procedure AdjustMONTHandYEAR(
+PROCEDURE AdjustMONTHandYEAR(
             VAR Mfile,Yfile : INTEGER);
         external 'aquacrop' name '__ac_tempprocessing_MOD_adjustmonthandyear';
 
-procedure AdjustDecadeMONTHandYEAR(
+PROCEDURE AdjustDecadeMONTHandYEAR(
             VAR DecFile,Mfile,Yfile : INTEGER);
         external 'aquacrop' name '__ac_tempprocessing_MOD_adjustdecademonthandyear';
-
-procedure SetDayNrToYundef(
-            VAR DayNri : LongInt);
-        external 'aquacrop' name '__ac_tempprocessing_MOD_setdaynrtoyundef';
-
-procedure GetDecadeTemperatureDataSet(
-            constref DayNri : LongInt;
-            VAR TminDataSet,TmaxDataSet : rep_SimulationEventsDbl);
-        external 'aquacrop' name '__ac_tempprocessing_MOD_getdecadetemperaturedataset';
-
-procedure GetMonthlyTemperatureDataSet(
-            constref DayNri : LongInt;
-            VAR TminDataSet,TmaxDataSet : rep_SimulationEventsDbl);
-        external 'aquacrop' name '__ac_tempprocessing_MOD_getmonthlytemperaturedataset';
-
 
 implementation
 
