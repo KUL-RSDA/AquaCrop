cadabec239ad0f4540e5640b7847ffdf6bfd0e42
MaximeVdB <maxime.cp.vandenbossche@gmail.com>
2022-02-10 13:44:38 +0100

    TST: use tolerance of unit-deviation-in-least-significant-digit
    Also replaces some of the assert statements with a "myassert" wrapper.
    This is to ensure that, once changed into the temporary working
    directory, that we're changing back to the original working directory
    also when the test is failing.

--- tests/test_europe.py
+++ tests/test_europe.py
@@ -6,6 +6,65 @@ import pytest
 import numpy as np
 
 
+def is_within_one_significant_digit(str_ref, str_val):
+    """
+    Returns whether two floating point numbers (given as strings)
+    have at most a unit difference in the least significant digit
+    (see the test_digits_check() test).
+    """
+    assert len(str_ref) == len(str_ref.strip()), str_ref
+    assert len(str_val) == len(str_val.strip()), str_val
+
+    if '.' in str_ref:
+        assert '.' in str_val, (str_ref, str_val)
+        # Check that both have same number of digits after the decimal
+        num_ref = len(str_ref) - str_ref.index('.') - 1
+        num_val = len(str_val) - str_val.index('.') - 1
+        assert num_ref == num_val, (str_ref, str_val)
+    else:
+        assert '.' not in str_val, (str_ref, str_val)
+
+    int_ref = int(str_ref.replace('.', '', 1))
+    int_val = int(str_val.replace('.', '', 1))
+    is_ok = abs(int_ref - int_val) < 2
+    return is_ok
+
+
+def myassert(condition, message, root_dir):
+    """
+    Wrapper for the 'assert' function, where the current working
+    directory is changed before raising the AssertionError.
+    """
+    if not condition:
+        os.chdir(root_dir)
+    assert condition, message
+    return
+
+
+@pytest.mark.parametrize('param', [
+    ('09', '10', True),
+    ('9', '10', True),
+    ('9.9', '10.0', True),
+    ('14.9', '15.0', True),
+    ('15.361', '15.362', True),
+    ('15.359', '15.360', True),
+    ('15.300', '15.299', True),
+    ('90', '99', False),
+    ('9.0', '10.0', False),
+    ('14.9', '15.1', False),
+    ('15.361', '15.363', False),
+    ('15.371', '15.361', False),
+])
+def test_digits_check(param):
+    str_ref, str_val, expected = param
+    result = is_within_one_significant_digit(str_ref, str_val)
+    is_ok = result == expected
+    print('Checking {0} vs {1}: {2} == {3} = {4}'.format(
+                str_ref, str_val, expected, result, 'OK' if is_ok else 'FAIL'))
+    assert is_ok, (str_ref, str_val, expected, result)
+    return
+
+
 @pytest.mark.parametrize('row', range(51))
 @pytest.mark.parametrize('col', range(105))
 @pytest.mark.parametrize('use_irrigation', [False, True])
@@ -88,7 +147,9 @@ def test_europe(row, col, use_irrigation):
     exitcode = os.system(cmd)
     t_stop = time.time()
     print('Runtime: {0:.1f} seconds'.format(t_stop - t_start))
-    assert exitcode == 0, 'AquaCrop exited with exit code {0}'.format(exitcode)
+    myassert(exitcode == 0,
+             'AquaCrop exited with exit code {0}'.format(exitcode),
+             cwd)
 
     # Remove extra files and directories: only save PRM and output file
     shutil.rmtree(os.path.join(pixel_dir, 'SIMUL'))
@@ -109,28 +170,69 @@ def test_europe(row, col, use_irrigation):
         with open(output_file, 'r', encoding='ISO-8859-1') as f:
             out_lines = f.readlines()
 
-        assert len(ref_lines) == len(out_lines), \
-               ('Different number of lines in output and reference files ',
-                reference_file, output_file)
+        myassert(len(ref_lines) == len(out_lines), \
+                 ('Different number of lines in output and reference files ',
+                  reference_file, output_file),
+                 cwd)
+
+        num_col = {'day': 114, 'season': 41}[suffix]
+        num_not_realclose = [0] * num_col
+        num_items_total = [0] * num_col
 
         for i, (ref_line, out_line) in enumerate(zip(ref_lines, out_lines)):
             if i == 0:
                 # Only elementary checks for the header line
-                assert ref_line.startswith('AquaCrop'), (i, ref_line, out_line)
-                assert out_line.startswith('AquaCrop'), (i, ref_line, out_line)
-                assert 'Output created on' in out_line, (i, ref_line, out_line)
-                assert 'Output created on' in ref_line, (i, ref_line, out_line)
+                myassert(ref_line.startswith('AquaCrop'),
+                         (i, ref_line, out_line), cwd)
+                myassert(out_line.startswith('AquaCrop'),
+                         (i, ref_line, out_line), cwd)
+                myassert('Output created on' in out_line,
+                         (i, ref_line, out_line), cwd)
+                myassert('Output created on' in ref_line,
+                         (i, ref_line, out_line), cwd)
             else:
+                items_ref = ref_line.split()
+                for i in range(len(items_ref)):
+                    num_items_total[i] += 1
+
                 try:
                     assert ref_line == out_line, (i, ref_line, out_line)
                 except AssertionError:
                     print('WARN: need item-by-item check (line {0})'.format(i))
-                    items_ref = ref_line.split()
                     items_out = out_line.split()
-                    for item_ref, item_out in zip(items_ref, items_out):
-                        if item_ref != item_out:
-                            assert np.isclose(float(item_ref), float(item_out))
 
+                    for icol, (item_ref, item_out) in enumerate(zip(items_ref,
+                                                                    items_out)):
+                        if item_ref != item_out:
+                            is_realclose = np.isclose(float(item_ref),
+                                                      float(item_out))
+
+                            if not is_realclose:
+                                num_not_realclose[icol] += 1
+
+                                try:
+                                    is_close = is_within_one_significant_digit(
+                                                            item_ref, item_out)
+                                except AssertionError:
+                                    myassert(False, ('failure checking digits',
+                                             icol, item_ref, item_out), cwd)
+                                else:
+                                    myassert(is_close, (icol, item_ref,
+                                             item_out), cwd)
+
+
+        print('{0} num_items_total = {1}'.format(filename,
+                                                 sum(num_items_total)))
+        print('{0} num_not_realclose = {1}'.format(filename,
+                                                   sum(num_not_realclose)))
+        numdevlist = [i * 100. / j for (i, j) in zip(num_not_realclose,
+                                                 num_items_total)]
+        print('{0} numdevlist [%] = {1}'.format(filename,
+                        ' '.join(map(lambda x: '%.3f' % x, numdevlist))))
+        maxdev = np.max(numdevlist)
+        maxcol = np.argmax(numdevlist)
+        print('{0} maxnumdev [%] = {1:.3f} (column index {2})'.format(filename,
+                                                               maxdev, maxcol))
         print('{0} checks = OK'.format(filename))
 
     os.chdir(cwd)
