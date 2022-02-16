1f1b7221372033d420b4d574e2c498d037878a4a
vsc32926 <vsc32926@tier2-p-login-3.genius.hpc.kuleuven.be>
2022-02-16 11:37:07 +0100

    edit: global_pas

--- src/Global.pas
+++ src/Global.pas
@@ -30,14 +30,13 @@ TYPE
 VAR DataPath,ObsPath : BOOLEAN;
     SWCiniFileFull,ProjectFileFull,MultipleProjectFileFull : string;
     ClimDescription,
-    IrriDescription,ManDescription,SWCiniDescription,
+    IrriDescription,ManDescription,
     ProjectDescription,MultipleProjectDescription,OffSeasonDescription,GroundWaterDescription: string;
 
     ClimRecord,
     EToRecord,
     RainRecord     : rep_clim;
     IrriFirstDayNr : LongInt;
-    NrCompartments : INTEGER;
     RootingDepth   : double;
     CCiActual,CCiPrev,CCiTopEarlySen : double;
 
@@ -100,7 +99,7 @@ PROCEDURE CalculateAdjustedFC(DepthAquifer : double;
                               VAR CompartAdj   : rep_Comp);
 PROCEDURE DesignateSoilLayerToCompartments(NrCompartments,NrSoilLayers : INTEGER;
                                           VAR Compartment : rep_Comp);
-PROCEDURE DeclareInitialCondAtFCandNoSalt;
+
 PROCEDURE specify_soil_layer(NrCompartments,NrSoilLayers : INTEGER;
                              VAR SoilLayer : rep_SoilLayer;
                              VAR Compartment : rep_Comp;
@@ -905,41 +904,6 @@ FOR i := (NrCompartments+1) TO max_No_compartments DO Compartment[i].Thickness :
 END; (* DesignateSoilLayerToCompartments *)
 
 
-PROCEDURE DeclareInitialCondAtFCandNoSalt;
-VAR layeri,compi,celli, ind : INTEGER;
-BEGIN
-SetSWCiniFile('(None)');
-SetSWCiniFileFull(GetSWCiniFile()); (* no file *)
-SWCiniDescription := 'Soil water profile at Field Capacity';
-SetSimulation_IniSWC_AtDepths(false);
-SetSimulation_IniSWC_NrLoc(GetSoil().NrSoilLayers);
-FOR layeri := 1 TO GetSoil().NrSoilLayers DO
-    BEGIN
-    SetSimulation_IniSWC_Loc_i(layeri,GetSoilLayer_i(layeri).Thickness);
-    SetSimulation_IniSWC_VolProc_i(layeri,GetSoilLayer_i(layeri).FC);
-    SetSimulation_IniSWC_SaltECe_i(layeri,0);
-    END;
-SetSimulation_IniSWC_AtFC(true);
-FOR layeri := (GetSoil().NrSoilLayers+1) TO max_No_compartments DO
-    BEGIN
-    SetSimulation_IniSWC_Loc_i(layeri,undef_double);
-    SetSimulation_IniSWC_VolProc_i(layeri,undef_double);
-    SetSimulation_IniSWC_SaltECe_i(layeri,undef_double);
-    END;
-FOR compi := 1 TO NrCompartments DO
-    IF (GetCompartment_Layer(compi) = 0) THEN ind := 1 //LB: added an if statement to avoid having index=0
-    ELSE ind := GetCompartment_Layer(compi);
-    For celli := 1 TO GetSoilLayer_i(ind).SCP1 DO
-        BEGIN // salinity in cells
-        SetCompartment_Salt(compi, celli, 0.0);
-        SetCompartment_Depo(compi, celli, 0.0);
-        END;
-END;(* DeclareInitialCondAtFCandNoSalt *)
-
-
-
-
-
 PROCEDURE specify_soil_layer(NrCompartments,NrSoilLayers : INTEGER;
                              VAR SoilLayer : rep_SoilLayer;
                              VAR Compartment : rep_Comp;
