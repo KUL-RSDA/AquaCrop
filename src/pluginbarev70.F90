62be4483cc63de6cd4c4954b3f1130c45bd07c95
MaximeVdB <maxime.cp.vandenbossche@gmail.com>
2022-03-17 11:04:15 +0100

    ENH: replace RunSimulation call by own time loop control

--- src/pluginbarev70.F90
+++ src/pluginbarev70.F90
@@ -1,7 +1,10 @@
 program pluginbarev70
 
-use ac_global, only: int32, &
-                     intEnum
+use ac_global, only: int8, &
+                     int32, &
+                     intEnum, &
+                     typeproject_typepro, &
+                     typeproject_typeprm
 use ac_interface_global, only: pointer2string, &
                                string2pointer
 use, intrinsic :: iso_c_binding, only: c_ptr
@@ -9,6 +12,8 @@ implicit none
 
 
 interface
+    ! Interface to the procedures export in the external AquaCrop library
+
     subroutine StartTheProgram()
     end subroutine StartTheProgram
 
@@ -54,21 +59,64 @@ interface
         integer(intEnum), intent(in) :: TheProjectType
     end subroutine InitializeProject_wrap
 
+    function GetDayNri() result(DayNri)
+        integer :: DayNri
+    end function GetDayNri
+
     subroutine RunSimulation_wrap(TheProjectFile, TheProjectType)
         import c_ptr, intEnum
         type(c_ptr), intent(in) :: TheProjectFile
         integer(intEnum), intent(in) :: TheProjectType
     end subroutine RunSimulation_wrap
+
+    subroutine InitializeSimulation_wrap(TheProjectFile, TheProjectType)
+        import c_ptr, intEnum
+        type(c_ptr), intent(in) :: TheProjectFile
+        integer(intEnum), intent(in) :: TheProjectType
+    end subroutine InitializeSimulation_wrap
+
+    subroutine InitializeRun_wrap(NrRun, TheProjectType)
+        import int8, intEnum
+        integer(int8), intent(in) :: NrRun
+        integer(intEnum), intent(in) :: TheProjectType
+    end subroutine InitializeRun_wrap
+
+    subroutine AdvanceOneTimeStep()
+    end subroutine AdvanceOneTimeStep
+
+    subroutine FinalizeRun1_wrap(NrRun, TheProjectFile, TheProjectType)
+        import c_ptr, int8, intEnum
+        integer(int8), intent(in) :: NrRun
+        type(c_ptr), intent(in) :: TheProjectFile
+        integer(intEnum), intent(in) :: TheProjectType
+    end subroutine FinalizeRun1_wrap
+
+    subroutine FinalizeRun2_wrap(NrRun, TheProjectType)
+        import int8, intEnum
+        integer(int8), intent(in) :: NrRun
+        integer(intEnum), intent(in) :: TheProjectType
+    end subroutine FinalizeRun2_wrap
+
+    subroutine FinalizeSimulation()
+    end subroutine FinalizeSimulation
+
+    function GetSimulation_NrRuns() result(NrRuns)
+        integer :: NrRuns
+    end function GetSimulation_NrRuns
+
+    function GetSimulation_ToDayNr() result(ToDayNr)
+        integer :: ToDayNr
+    end function GetSimulation_ToDayNr
 end interface
 
 
-integer :: iproject, nprojects
-character(len=:), allocatable :: ListProjectsFile, TheProjectFile
-logical :: ListProjectFileExist
+integer :: daynr, todaynr, iproject, nprojects, irun, nruns
 integer(intEnum) :: TheProjectType
+logical :: ListProjectFileExist
+character(len=:), allocatable :: ListProjectsFile, TheProjectFile
 
-!to run AquaCrop in "standalone" mode:
-!call StartTheProgram()
+! All lines below are the equivalent of
+! call StartTheProgram()
 
 call InitializeTheProgram()
 
@@ -85,7 +133,34 @@ do iproject = 1, nprojects
     TheProjectFile = GetProjectFileName(iproject)
     call GetProjectType(TheProjectFile, TheProjectType)
     call InitializeProject(iproject, TheProjectFile, TheProjectType)
-    call RunSimulation(TheProjectFile, TheProjectType)
+
+    ! The remainder of this loop is the equivalent of
+    ! call RunSimulation(TheProjectFile, TheProjectType)
+
+    call InitializeSimulation(TheProjectFile, TheProjectType)
+
+    if (TheProjectType == typeproject_typepro) then
+        nruns = 1
+    elseif (TheProjectType == typeproject_typeprm) then
+        nruns = GetSimulation_NrRuns()
+    end if
+
+    do irun = 1, nruns
+        call InitializeRun(irun, TheProjectType);
+
+        todaynr = GetSimulation_ToDayNr()
+
+        time_loop: do
+            call AdvanceOneTimeStep()
+            daynr = GetDayNri()
+            if ((daynr - 1) == todaynr) exit time_loop
+        end do time_loop
+
+        call FinalizeRun1(irun, TheProjectFile, TheProjectType);
+        call FinalizeRun2(irun, TheProjectType);
+    end do
+
+    call FinalizeSimulation();
 end do
 
 if (nprojects == 0) then
@@ -170,5 +245,44 @@ subroutine RunSimulation(TheProjectFile, TheProjectType)
     c_pointer = string2pointer(TheProjectFile)
     call RunSimulation_wrap(c_pointer, TheProjectType)
 end subroutine RunSimulation
+
+
+subroutine InitializeSimulation(TheProjectFile, TheProjectType)
+    character(len=*), intent(in) :: TheProjectFile
+    integer(intEnum), intent(in) :: TheProjectType
+
+    type(c_ptr) :: c_pointer
+
+    c_pointer = string2pointer(TheProjectFile)
+    call InitializeSimulation_wrap(c_pointer, TheProjectType)
+end subroutine InitializeSimulation
+
+
+subroutine InitializeRun(NrRun, TheProjectType)
+    integer, intent(in) :: NrRun
+    integer(intEnum), intent(in) :: TheProjectType
+
+    call InitializeRun_wrap(int(NrRun, kind=int8), TheProjectType)
+end subroutine InitializeRun
+
+
+subroutine FinalizeRun1(NrRun, TheProjectFile, TheProjectType)
+    integer, intent(in) :: NrRun
+    character(len=*), intent(in) :: TheProjectFile
+    integer(intEnum), intent(in) :: TheProjectType
+
+    type(c_ptr) :: c_pointer
+
+    c_pointer = string2pointer(TheProjectFile)
+    call FinalizeRun1_wrap(int(NrRun, kind=int8), c_pointer, TheProjectType)
+end subroutine FinalizeRun1
+
+
+subroutine FinalizeRun2(NrRun, TheProjectType)
+    integer, intent(in) :: NrRun
+    integer(intEnum), intent(in) :: TheProjectType
+
+    call FinalizeRun2_wrap(int(NrRun, kind=int8), TheProjectType)
+end subroutine FinalizeRun2
 
 end program pluginbarev70
