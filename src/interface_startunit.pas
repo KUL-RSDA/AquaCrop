a45c7f9ce43e931bfc2dbe14a906ff6181e8a6b2
vsc32926 <vsc32926@tier2-p-login-4.genius.hpc.kuleuven.be>
2022-03-31 10:32:37 +0200

    ENH: convert startunit GetTimeAggregationResults

--- src/interface_startunit.pas
+++ src/interface_startunit.pas
@@ -4,7 +4,8 @@ interface
 
 uses Global, interface_global, InitialSettings, interface_initialsettings, Run, interface_run;
 
-
+procedure GetTimeAggregationResults(VAR OutputAggregate : ShortInt);
+    external 'aquacrop' name '__ac_startunit_MOD_gettimeaggregationresults';
 
 
 implementation
--- src/startunit.f90
+++ src/startunit.f90
@@ -1,11 +1,52 @@
 module ac_startunit
 
-use ac_kinds, only: int32
+use ac_kinds, only: int8, &
+                    int32
+
+use ac_global, only: FileExists, &
+                     GetPathNameSimul
+
 implicit none
 
 
 contains
 
+subroutine GetTimeAggregationResults(OutputAggregate)
+    integer(int8), intent(inout) :: OutputAggregate
+
+    integer :: fhandle
+    character(len=1025) :: FullFileName, TempString
+
+    integer(int32) :: n, i
+
+    OutputAggregate = 0 ! simulation period 0: season
+    FullFileName = GetPathNameSimul() // 'AggregationResults.SIM'
+    if (FileExists(FullFileName) .eqv. .true.) then
+        open(newunit=fhandle, file=trim(FullFileName), status='old', action='read')
+        read(fhandle, *) TempString
+        n = len(TempString)
+        if (n > 0) then
+            i = 1
+            do while ((TempString(i:i) == ' ') .and. (i < n))
+                i = i + 1
+            end do
+            if (TempString(i:i) == '1') then
+                OutputAggregate = 1 ! 1: daily aggregation
+            else
+                if (TempString(i:i) == '2') then
+                    OutputAggregate = 2 ! 2 : 10-daily aggregation
+                else
+                    if (TempString(i:i) == '3') then
+                        OutputAggregate = 3 ! 3 : monthly aggregation
+                    else
+                        OutputAggregate = 0 ! 0 : seasonal results only
+                    end if
+                end if
+            end if
+        end if
+        close(fhandle)
+    end if
+end subroutine GetTimeAggregationResults
 
 
 end module ac_startunit
