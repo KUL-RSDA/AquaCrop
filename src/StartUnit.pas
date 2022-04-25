04268412225aa0dec2b467b9b483facb0eeb24c6
vsc32926 <vsc32926@tier2-p-login-3.genius.hpc.kuleuven.be>
2022-04-25 12:50:23 +0200

    edit

--- src/StartUnit.pas
+++ src/StartUnit.pas
@@ -42,45 +42,4 @@ IF (lgth > 0) THEN
 END; // GetProjectType
 
 
-PROCEDURE StartTheProgram;
-VAR
-    iproject, nprojects : integer;
-    ListProjectsFile, TheProjectFile : string;
-    ListProjectFileExist : boolean;
-    TheProjectType : repTypeProject;
-
-BEGIN
-    InitializeTheProgram;
-
-    ListProjectsFile := GetListProjectsFile();
-    ListProjectFileExist := FileExists(ListProjectsFile);
-    nprojects := GetNumberOfProjects();
-
-    IF (nprojects > 0) THEN
-    BEGIN
-        WriteProjectsInfo('');
-        WriteProjectsInfo('Projects handled:');
-    END;
-
-    FOR iproject := 1 to nprojects DO
-    BEGIN
-        TheProjectFile := GetProjectFileName(iproject);
-        GetProjectType(TheProjectFile, TheProjectType);
-        InitializeProject(iproject, TheProjectFile, TheProjectType);
-        RunSimulation(TheProjectFile,TheProjectType);
-    END;
-
-    IF (nprojects = 0) THEN
-    BEGIN
-        WriteProjectsInfo('');
-        WriteProjectsInfo('Projects loaded: None');
-
-        IF ListProjectFileExist
-        THEN WriteProjectsInfo('File ''ListProjects.txt'' does not contain ANY project file')
-        ELSE WriteProjectsInfo('Missing File ''ListProjects.txt'' in LIST directory');
-    END;
-
-    FinalizeTheProgram;
-END;
-
 end.
