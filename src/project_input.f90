ac370bfff3f18c7ba567ae0edd8817b87cb5f248
MaximeVdB <maxime.cp.vandenbossche@gmail.com>
2022-06-23 11:50:53 +0200

    ENH: add allocators, getters and setters for convenience

--- src/project_input.f90
+++ src/project_input.f90
@@ -3,6 +3,7 @@ module ac_project_input
 use ac_kinds, only: dp, &
                     int8, &
                     int32
+use ac_utils, only: assert
 use iso_fortran_env, only: iostat_end
 implicit none
 
@@ -112,11 +113,40 @@ type ProjectInput_type
 end type ProjectInput_type
 
 
+interface get_project_input
+    module procedure get_project_input_dp
+    module procedure get_project_input_int8
+    module procedure get_project_input_int32
+    module procedure get_project_input_string
+end interface get_project_input
+
+
+interface set_project_input
+    module procedure set_project_input_dp
+    module procedure set_project_input_int8
+    module procedure set_project_input_int32
+    module procedure set_project_input_string
+end interface set_project_input
+
+
 type(ProjectInput_type), dimension(:), allocatable :: ProjectInput
     !! Project file input data for every run in a project
 
 
 contains
+
+
+subroutine allocate_project_input(NrRuns)
+    !! Simply (re)allocates the ProjectInput module variable,
+    integer(int32), intent(in) :: NrRuns
+        !! total number of runs
+
+    if (allocated(ProjectInput)) then
+        deallocate(ProjectInput)
+    end if
+
+    allocate(ProjectInput(NrRuns))
+end subroutine allocate_project_input
 
 
 subroutine initialize_project_input(filename, NrRuns)
@@ -136,7 +166,7 @@ subroutine initialize_project_input(filename, NrRuns)
     else
         call ReadNumberSimulationRuns(filename, NrRuns_local)
     end if
-    allocate(ProjectInput(NrRuns_local))
+    call allocate_project_input(NrRuns_local)
 
     do i = 1, NrRuns_local
         call ProjectInput(i)%read_project_file(filename, i)
@@ -341,5 +371,309 @@ subroutine read_project_file(self, filename, NrRun)
 
     close(fhandle)
 end subroutine read_project_file
+
+
+function get_project_input_dp(index, key, mold) result(value)
+    !! Returns the chosen double precision attribute of the
+    !! ProjectInput module variable.
+    integer, intent(in) :: index
+    character(len=*), intent(in) :: key
+    real(dp), intent(in) :: mold
+    real(dp) :: value
+
+    if (key == 'VersionNr') then
+        value = ProjectInput(index)%VersionNr
+    else
+        call assert(.false., 'Unknown dp key: ' // key)
+    end if
+end function get_project_input_dp
+
+
+subroutine set_project_input_dp(index, key, value)
+    !! Sets the chosen double precision attribute of the
+    !! ProjectInput module variable.
+    integer, intent(in) :: index
+    character(len=*), intent(in) :: key
+    real(dp), intent(in) :: value
+
+    if (key == 'VersionNr') then
+        ProjectInput(index)%VersionNr = value
+    else
+        call assert(.false., 'Unknown dp key: ' // key)
+    end if
+end subroutine set_project_input_dp
+
+
+function get_project_input_int8(index, key, mold) result(value)
+    !! Returns the chosen int8 attribute of the
+    !! ProjectInput module variable.
+    integer, intent(in) :: index
+    character(len=*), intent(in) :: key
+    integer(int8), intent(in) :: mold
+    integer(int8) :: value
+
+
+    if (key == 'Simulation_YearSeason') then
+        value = ProjectInput(index)%Simulation_YearSeason
+    else
+        call assert(.false., 'Unknown int8 key: ' // key)
+    end if
+end function get_project_input_int8
+
+
+subroutine set_project_input_int8(index, key, value)
+    !! Sets the chosen int8 attribute of the
+    !! ProjectInput module variable.
+    integer, intent(in) :: index
+    character(len=*), intent(in) :: key
+    integer(int8), intent(in) :: value
+
+    if (key == 'Simulation_YearSeason') then
+        ProjectInput(index)%Simulation_YearSeason = value
+    else
+        call assert(.false., 'Unknown int8 key: ' // key)
+    end if
+end subroutine set_project_input_int8
+
+
+function get_project_input_int32(index, key, mold) result(value)
+    !! Returns the chosen int32 attribute of the
+    !! ProjectInput module variable.
+    integer, intent(in) :: index
+    character(len=*), intent(in) :: key
+    integer(int32), intent(in) :: mold
+    integer(int32) :: value
+
+    if (key == 'Simulation_DayNr1') then
+        value = ProjectInput(index)%Simulation_DayNr1
+    elseif (key == 'Simulation_DayNrN') then
+        value = ProjectInput(index)%Simulation_DayNrN
+    elseif (key == 'Crop_Day1') then
+        value = ProjectInput(index)%Crop_Day1
+    elseif (key == 'Crop_DayN') then
+        value = ProjectInput(index)%Crop_DayN
+    else
+        call assert(.false., 'Unknown int32 key: ' // key)
+    end if
+end function get_project_input_int32
+
+
+subroutine set_project_input_int32(index, key, value)
+    !! Sets the chosen int32 attribute of the
+    !! ProjectInput module variable.
+    integer, intent(in) :: index
+    character(len=*), intent(in) :: key
+    integer(int32), intent(in) :: value
+
+    if (key == 'Simulation_DayNr1') then
+        ProjectInput(index)%Simulation_DayNr1 = value
+    elseif (key == 'Simulation_DayNrN') then
+        ProjectInput(index)%Simulation_DayNrN = value
+    elseif (key == 'Crop_Day1') then
+        ProjectInput(index)%Crop_Day1 = value
+    elseif (key == 'Crop_DayN') then
+        ProjectInput(index)%Crop_DayN = value
+    else
+        call assert(.false., 'Unknown int32 key: ' // key)
+    end if
+end subroutine set_project_input_int32
+
+
+function get_project_input_string(index, key) result(value)
+    !! Returns the chosen string attribute of the
+    !! ProjectInput module variable.
+    integer, intent(in) :: index
+    character(len=*), intent(in) :: key
+    character(len=:), allocatable :: value
+
+    if (key == 'Description') then
+        value = ProjectInput(index)%Description
+    elseif (key == 'Climate_Info') then
+        value = ProjectInput(index)%Climate_Info
+    elseif (key == 'Climate_Filename') then
+        value = ProjectInput(index)%Climate_Filename
+    elseif (key == 'Climate_Directory') then
+        value = ProjectInput(index)%Climate_Directory
+    elseif (key == 'Temperature_Info') then
+        value = ProjectInput(index)%Temperature_Info
+    elseif (key == 'Temperature_Filename') then
+        value = ProjectInput(index)%Temperature_Filename
+    elseif (key == 'Temperature_Directory') then
+        value = ProjectInput(index)%Temperature_Directory
+    elseif (key == 'ETo_Info') then
+        value = ProjectInput(index)%ETo_Info
+    elseif (key == 'ETo_Filename') then
+        value = ProjectInput(index)%ETo_Filename
+    elseif (key == 'ETo_Directory') then
+        value = ProjectInput(index)%ETo_Directory
+    elseif (key == 'Rain_Info') then
+        value = ProjectInput(index)%Rain_Info
+    elseif (key == 'Rain_Filename') then
+        value = ProjectInput(index)%Rain_Filename
+    elseif (key == 'Rain_Directory') then
+        value = ProjectInput(index)%Rain_Directory
+    elseif (key == 'CO2_Info') then
+        value = ProjectInput(index)%CO2_Info
+    elseif (key == 'CO2_Filename') then
+        value = ProjectInput(index)%CO2_Filename
+    elseif (key == 'CO2_Directory') then
+        value = ProjectInput(index)%CO2_Directory
+    elseif (key == 'Calendar_Info') then
+        value = ProjectInput(index)%Calendar_Info
+    elseif (key == 'Calendar_Filename') then
+        value = ProjectInput(index)%Calendar_Filename
+    elseif (key == 'Calendar_Directory') then
+        value = ProjectInput(index)%Calendar_Directory
+    elseif (key == 'Crop_Info') then
+        value = ProjectInput(index)%Crop_Info
+    elseif (key == 'Crop_Filename') then
+        value = ProjectInput(index)%Crop_Filename
+    elseif (key == 'Crop_Directory') then
+        value = ProjectInput(index)%Crop_Directory
+    elseif (key == 'Irrigation_Info') then
+        value = ProjectInput(index)%Irrigation_Info
+    elseif (key == 'Irrigation_Filename') then
+        value = ProjectInput(index)%Irrigation_Filename
+    elseif (key == 'Irrigation_Directory') then
+        value = ProjectInput(index)%Irrigation_Directory
+    elseif (key == 'Management_Info') then
+        value = ProjectInput(index)%Management_Info
+    elseif (key == 'Management_Filename') then
+        value = ProjectInput(index)%Management_Filename
+    elseif (key == 'Management_Directory') then
+        value = ProjectInput(index)%Management_Directory
+    elseif (key == 'GroundWater_Info') then
+        value = ProjectInput(index)%GroundWater_Info
+    elseif (key == 'GroundWater_Filename') then
+        value = ProjectInput(index)%GroundWater_Filename
+    elseif (key == 'GroundWater_Directory') then
+        value = ProjectInput(index)%GroundWater_Directory
+    elseif (key == 'Soil_Info') then
+        value = ProjectInput(index)%Soil_Info
+    elseif (key == 'Soil_Filename') then
+        value = ProjectInput(index)%Soil_Filename
+    elseif (key == 'Soil_Directory') then
+        value = ProjectInput(index)%Soil_Directory
+    elseif (key == 'SWCIni_Info') then
+        value = ProjectInput(index)%SWCIni_Info
+    elseif (key == 'SWCIni_Filename') then
+        value = ProjectInput(index)%SWCIni_Filename
+    elseif (key == 'SWCIni_Directory') then
+        value = ProjectInput(index)%SWCIni_Directory
+    elseif (key == 'OffSeason_Info') then
+        value = ProjectInput(index)%OffSeason_Info
+    elseif (key == 'OffSeason_Filename') then
+        value = ProjectInput(index)%OffSeason_Filename
+    elseif (key == 'OffSeason_Directory') then
+        value = ProjectInput(index)%OffSeason_Directory
+    elseif (key == 'Observations_Info') then
+        value = ProjectInput(index)%Observations_Info
+    elseif (key == 'Observations_Filename') then
+        value = ProjectInput(index)%Observations_Filename
+    elseif (key == 'Observations_Directory') then
+        value = ProjectInput(index)%Observations_Directory
+    else
+        call assert(.false., 'Unknown string key: ' // key)
+    end if
+end function get_project_input_string
+
+
+subroutine set_project_input_string(index, key, value)
+    !! Sets the chosen string attribute of the
+    !! ProjectInput module variable.
+    integer, intent(in) :: index
+    character(len=*), intent(in) :: key
+    character(len=*), intent(in) :: value
+
+    if (key == 'Description') then
+        ProjectInput(index)%Description = value
+    elseif (key == 'Climate_Info') then
+        ProjectInput(index)%Climate_Info = value
+    elseif (key == 'Climate_Filename') then
+        ProjectInput(index)%Climate_Filename = value
+    elseif (key == 'Climate_Directory') then
+        ProjectInput(index)%Climate_Directory = value
+    elseif (key == 'Temperature_Info') then
+        ProjectInput(index)%Temperature_Info = value
+    elseif (key == 'Temperature_Filename') then
+        ProjectInput(index)%Temperature_Filename = value
+    elseif (key == 'Temperature_Directory') then
+        ProjectInput(index)%Temperature_Directory = value
+    elseif (key == 'ETo_Info') then
+        ProjectInput(index)%ETo_Info = value
+    elseif (key == 'ETo_Filename') then
+        ProjectInput(index)%ETo_Filename = value
+    elseif (key == 'ETo_Directory') then
+        ProjectInput(index)%ETo_Directory = value
+    elseif (key == 'Rain_Info') then
+        ProjectInput(index)%Rain_Info = value
+    elseif (key == 'Rain_Filename') then
+        ProjectInput(index)%Rain_Filename = value
+    elseif (key == 'Rain_Directory') then
+        ProjectInput(index)%Rain_Directory = value
+    elseif (key == 'CO2_Info') then
+        ProjectInput(index)%CO2_Info = value
+    elseif (key == 'CO2_Filename') then
+        ProjectInput(index)%CO2_Filename = value
+    elseif (key == 'CO2_Directory') then
+        ProjectInput(index)%CO2_Directory = value
+    elseif (key == 'Calendar_Info') then
+        ProjectInput(index)%Calendar_Info = value
+    elseif (key == 'Calendar_Filename') then
+        ProjectInput(index)%Calendar_Filename = value
+    elseif (key == 'Calendar_Directory') then
+        ProjectInput(index)%Calendar_Directory = value
+    elseif (key == 'Crop_Info') then
+        ProjectInput(index)%Crop_Info = value
+    elseif (key == 'Crop_Filename') then
+        ProjectInput(index)%Crop_Filename = value
+    elseif (key == 'Crop_Directory') then
+        ProjectInput(index)%Crop_Directory = value
+    elseif (key == 'Irrigation_Info') then
+        ProjectInput(index)%Irrigation_Info = value
+    elseif (key == 'Irrigation_Filename') then
+        ProjectInput(index)%Irrigation_Filename = value
+    elseif (key == 'Irrigation_Directory') then
+        ProjectInput(index)%Irrigation_Directory = value
+    elseif (key == 'Management_Info') then
+        ProjectInput(index)%Management_Info = value
+    elseif (key == 'Management_Filename') then
+        ProjectInput(index)%Management_Filename = value
+    elseif (key == 'Management_Directory') then
+        ProjectInput(index)%Management_Directory = value
+    elseif (key == 'GroundWater_Info') then
+        ProjectInput(index)%GroundWater_Info = value
+    elseif (key == 'GroundWater_Filename') then
+        ProjectInput(index)%GroundWater_Filename = value
+    elseif (key == 'GroundWater_Directory') then
+        ProjectInput(index)%GroundWater_Directory = value
+    elseif (key == 'Soil_Info') then
+        ProjectInput(index)%Soil_Info = value
+    elseif (key == 'Soil_Filename') then
+        ProjectInput(index)%Soil_Filename = value
+    elseif (key == 'Soil_Directory') then
+        ProjectInput(index)%Soil_Directory = value
+    elseif (key == 'SWCIni_Info') then
+        ProjectInput(index)%SWCIni_Info = value
+    elseif (key == 'SWCIni_Filename') then
+        ProjectInput(index)%SWCIni_Filename = value
+    elseif (key == 'SWCIni_Directory') then
+        ProjectInput(index)%SWCIni_Directory = value
+    elseif (key == 'OffSeason_Info') then
+        ProjectInput(index)%OffSeason_Info = value
+    elseif (key == 'OffSeason_Filename') then
+        ProjectInput(index)%OffSeason_Filename = value
+    elseif (key == 'OffSeason_Directory') then
+        ProjectInput(index)%OffSeason_Directory = value
+    elseif (key == 'Observations_Info') then
+        ProjectInput(index)%Observations_Info = value
+    elseif (key == 'Observations_Filename') then
+        ProjectInput(index)%Observations_Filename = value
+    elseif (key == 'Observations_Directory') then
+        ProjectInput(index)%Observations_Directory = value
+    else
+        call assert(.false., 'Unknown string key: ' // key)
+    end if
+end subroutine set_project_input_string
 
 end module ac_project_input
